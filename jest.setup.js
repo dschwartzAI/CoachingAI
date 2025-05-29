6f82042ebbbcfcf5939dbe203b618938e81e8386
dschwartzAI <dan@getaidriven.com>
2025-05-28 17:44:27 -0700

    Fix tests and lint scripts

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom';
+
+// Provide TextEncoder/TextDecoder in Node test environment
+if (typeof global.TextEncoder === 'undefined') {
+  const { TextEncoder, TextDecoder } = require('util');
+  global.TextEncoder = TextEncoder;
+  global.TextDecoder = TextDecoder;
+}
