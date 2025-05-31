b45c6219ad59f713a8b457f58158a8a6d0341fcb
Daniel Schwartz <dschwartz06@gmail.com>
2025-05-30 18:09:49 -0700

    Fix PostHog pageview tracking timing issue

--- components/PostHogProvider.js
+++ components/PostHogProvider.js
@@ -7,15 +7,37 @@ import { PostHogProvider as PHProvider, usePostHog } from 'posthog-js/react'
 
 export function PostHogProvider({ children }) {
   useEffect(() => {
+    // Add console logs to debug initialization
+    console.log('[PostHog] Initializing with key:', process.env.NEXT_PUBLIC_POSTHOG_KEY ? 'Present' : 'Missing')
+    console.log('[PostHog] Host:', process.env.NEXT_PUBLIC_POSTHOG_HOST)
+    
+    if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+      console.error('[PostHog] NEXT_PUBLIC_POSTHOG_KEY is missing!')
+      return
+    }
+
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
       person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-      // Enable debug mode in development
+      // Enable debug mode as recommended in PostHog docs
       loaded: (posthog) => {
-        posthog.debug() // Enable debug mode unconditionally for now
+        console.log('[PostHog] PostHog loaded successfully!')
+        console.log('[PostHog] PostHog instance:', posthog)
+        posthog.debug() // Enable debug mode as per PostHog documentation
+        console.log('[PostHog] Debug mode enabled')
+        
+        // Manually trigger a pageview after PostHog is loaded
+        const currentUrl = window.location.href
+        console.log('[PostHog] Sending initial pageview after load:', currentUrl)
+        posthog.capture('$pageview', {
+          '$current_url': currentUrl,
+          '$pathname': window.location.pathname
+        })
       }
     })
+    
+    console.log('[PostHog] Initialization attempted')
   }, [])
 
   return (
@@ -29,19 +51,37 @@ export function PostHogProvider({ children }) {
 function PostHogPageView() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
-  const { track } = usePostHog()
+  const posthog = usePostHog()
 
   // Track pageviews
   useEffect(() => {
-    if (pathname && track) {
+    console.log('[PostHog PageView] Effect triggered:', { 
+      pathname: !!pathname, 
+      hasPosthog: !!posthog,
+      posthogReady: posthog?._loaded
+    })
+    
+    if (pathname && posthog && posthog._loaded) {
       let url = window.origin + pathname
       if (searchParams.toString()) {
         url = url + "?" + searchParams.toString()
       }
 
-      track('$pageview', { '$current_url': url })
+      console.log('[PostHog PageView] Sending pageview for:', url)
+      // Use posthog.capture directly instead of the track wrapper
+      posthog.capture('$pageview', { 
+        '$current_url': url,
+        '$pathname': pathname
+      })
+      console.log('[PostHog PageView] Pageview sent successfully')
+    } else {
+      console.warn('[PostHog PageView] Not sending pageview:', { 
+        pathname: !!pathname, 
+        hasPosthog: !!posthog,
+        posthogLoaded: posthog?._loaded
+      })
     }
-  }, [pathname, searchParams, track])
+  }, [pathname, searchParams, posthog])
 
   return null
 }
