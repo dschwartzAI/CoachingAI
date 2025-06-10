5fc399dd25c2ec3f4e0b8f946658f15f12da3fdb
dschwartzAI <dan@getaidriven.com>
2025-06-09 18:39:22 -0700

    feat: centralize server supabase client

--- app/api/profile/route.js
+++ app/api/profile/route.js
@@ -1,26 +1,8 @@
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+import { createServerClientWithCookies } from '@/lib/utils/supabaseServer'
 import { NextResponse } from 'next/server'
 
 function createSupabaseClient() {
-  const cookieStore = cookies()
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    {
-      cookies: {
-        get(name) {
-          return cookieStore.get(name)?.value
-        },
-        set(name, value, options) {
-          cookieStore.set({ name, value, ...options })
-        },
-        remove(name, options) {
-          cookieStore.set({ name, value: '', ...options })
-        }
-      }
-    }
-  )
+  return createServerClientWithCookies()
 }
 
 export async function GET() {
